e976bd78ddd66e9436690156490bf8d262c579d4
Divya Rachel <divyadeepti2000@gmail.com>
2019-10-27 00:25:58 +0530

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+function promt(){
+var prom=prompt("Your name madam?");
+if(prom=="Divya"){
+document.write("Welcome back");
+}
+else
+    document.write("Access denied")
+}
+/*setInterval("promt()",1000);*/
